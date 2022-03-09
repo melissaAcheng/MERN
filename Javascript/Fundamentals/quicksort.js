9ec2303ae835255666b64cafeb7f2bab8b090a3b
melissaAcheng <melcheng12@gmail.com>
2022-03-09 10:32:20 -0500

    completed

--- Javascript/Fundamentals/quicksort.js
+++ Javascript/Fundamentals/quicksort.js
@@ -1,33 +1,30 @@
 const nums = [4,8,5,7,1,6,10,2,3,9];
 
 function quickSort(arr, start, end) {
-
-    if (start >= end) {
-        return;
+    if (start < end) {
+        let index = partition(arr, start, end);
+        quickSort(arr, start, index-1);
+        quickSort(arr, index+1, end);
     }
-
-    let index = partition(arr, start, end);
-    
-    quickSort(arr, start, index-1);
-    quickSort(arr, index+1, end);
-    
+    return arr;
 }
 
 function partition(arr, start, end) {
-
     let pivotIndex = randomIndex(start,end);
     let pivotValue = arr[pivotIndex];
-
+    
     while (start < end) {
-        if (arr[start] < pivotValue) {
+        while (arr[start] < pivotValue) {
             start++;
-        } if (arr[end] > pivotValue) {
+        } 
+        while (arr[end] > pivotValue) {
             end--;
-        } if (arr[start] >= pivotValue && arr[end] <= pivotValue) {
+        } 
+        if (arr[start] >= pivotValue && arr[end] <= pivotValue) {
             [arr[start], arr[end]] = [arr[end], arr[start]];
-        }
+        } 
     }
-    return pivotIndex;
+    return end;
 }
 
 console.log(quickSort(nums, 0, nums.length-1));
@@ -36,7 +33,6 @@ function randomIndex(start, end) {
     return Math.floor(Math.random() * (end - start + 1) + start);
 }
 
-// console.log(randomIndex(0, 7));
 
 
 
