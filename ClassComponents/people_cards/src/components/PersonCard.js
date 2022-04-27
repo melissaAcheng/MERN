3f1eef6f9ce75d865f76fddc6db76bfa7a78450b
melissaAcheng <melcheng12@gmail.com>
2022-04-26 22:53:48 -0400

    added state to increment age on click of button

--- ClassComponents/people_cards/src/components/PersonCard.js
+++ ClassComponents/people_cards/src/components/PersonCard.js
@@ -1,15 +1,30 @@
 import React, { Component } from "react";
 
 class PersonCard extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      age: this.props.age,
+    };
+  }
+
+  addOne = () => {
+    this.setState({ age: this.state.age + 1 });
+    console.log(this.state.age);
+  };
+
   render() {
-    const { firstName, lastName, age, hairColor} = this.props;
+    const { firstName, lastName, age, hairColor } = this.props;
     return (
       <div>
         <h1>
           {lastName}, {firstName}
         </h1>
-        <p>Age: {age}</p>
+        <p>Age: {this.state.age}</p>
         <p>Hair Color: {hairColor}</p>
+        <button onClick={this.addOne}>
+          Birthday Button for {firstName} {lastName}
+        </button>
       </div>
     );
   }
